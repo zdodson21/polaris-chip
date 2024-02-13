5497a80e15deb3e225251476e5c92d5887c71a03
zdodson21 <zdodson21@gmail.com>
2024-02-13 13:07:03 -0500

    initial counter structure, will attempt logic

--- src/counter-app.js
+++ src/counter-app.js
@@ -17,26 +17,51 @@ export class CounterApp extends LitElement {
         return css`
             :host {
                 display: block;
+                width: 400px;
+                background-color: #121212;
+                color: white;
+            }
+            
+            .buttons {
+                display: inline-flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
             }
 
-
+            .btn {
+                font-size: 48px;
+                width: 60px;
+                height: 60px;
+                margin-bottom: 15px;
+            }
         `;
     }
 
     render() {
         return html `
-            <h1>Counter</h1>
-            <p>${this.counter}</p>
+            <h2 style='text-align: center; font-size: 60px;'>Counter</h2>
+            <p style='text-align: center; font-size: 48px;'>${this.counter}</p>
+            <div class='buttons'>
+                <button class='btn' id='minus' style='margin-right: 15px;'>-</button> 
+                <button class='btn' id='plus' style='margin-left: 15px;'>+</button>
+            </div>
         `;
     }
 
     static get properties() {
         return {
-            counter: { type: int },
-            min: { type: int},
-            max: { type: int},
+            counter: { type: Number },
+            min: { type: Number },
+            max: { type: Number },
         };
     }
 }
 
 globalThis.customElements.define(CounterApp.tag, CounterApp);
+
+// button logic???
+
+document.querySelector('#minus').addEventListener('click', (e) => {
+    console.log('minus pressed');
+})
