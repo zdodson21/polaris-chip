4ca023b9d493ecf8a1b27d6216172ecb49978194
zdodson21 <zdodson21@gmail.com>
2024-02-15 12:56:44 -0500

    got confetti to work (yayyy)

--- src/counter-app.js
+++ src/counter-app.js
@@ -118,32 +118,32 @@ export class CounterApp extends LitElement {
         }
     }
 
-    // updated(changedProperties) {
-    //     console.log(changedProperties);
-    //     if (changedProperties.has('counter') === 21) {
-    //       this.makeItRain;
-    //     }
-    // }
+    updated(changedProperties) {
+        if (changedProperties.has('counter') && this.counter === 18) {
+          // do your testing of the value and make it rain by calling makeItRain
+          this.makeItRain();
+        }
+      }
       
-    // makeItRain() {
-    //     // this is called a dynamic import. It means it won't import the code for confetti until this method is called
-    //     // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
-    //     // will only run AFTER the code is imported and available to us
-    //     import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
-    //         (module) => {
-    //         // This is a minor timing 'hack'. We know the code library above will import prior to this running
-    //         // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
-    //         // this "hack" ensures the element has had time to process in the DOM so that when we set popped
-    //         // it's listening for changes so it can react
-    //         setTimeout(() => {
-    //             // forcibly set the poppped attribute on something with id confetti
-    //             // while I've said in general NOT to do this, the confetti container element will reset this
-    //             // after the animation runs so it's a simple way to generate the effect over and over again
-    //             this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
-    //         }, 0);
-    //         }
-    //     );
-    // }
+      makeItRain() {
+        // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+        // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+        // will only run AFTER the code is imported and available to us
+        import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+          (module) => {
+            // This is a minor timing 'hack'. We know the code library above will import prior to this running
+            // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+            // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+            // it's listening for changes so it can react
+            setTimeout(() => {
+              // forcibly set the poppped attribute on something with id confetti
+              // while I've said in general NOT to do this, the confetti container element will reset this
+              // after the animation runs so it's a simple way to generate the effect over and over again
+              this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+            }, 0);
+          }
+        );
+      }
 
     static get properties() {
         return {
