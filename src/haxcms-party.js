211f7d6972a1f8efc06a2c0aee85cd3e4212b296
zdodson21 <zdodson21@gmail.com>
2024-03-19 13:04:56 -0400

    imports d-d-d properly now

--- src/haxcms-party.js
+++ src/haxcms-party.js
@@ -1,6 +1,7 @@
 import { html, css } from 'lit';
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 import "@lrnwebcomponents/rpg-character/rpg-character.js";
-import { DDD } from "@lrnwebcomponents/d-d-d";
+
 
 
 export class HaxCMSParty extends DDD {
