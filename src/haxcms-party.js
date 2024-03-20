8df2f76d06ee8a4d0bc7a70ee0783153cefabb92
zdodson21 <zdodson21@gmail.com>
2024-03-20 18:18:43 -0400

    basic content for addUser(), may still have syntax  error, but cannot test at moment

--- src/haxcms-party.js
+++ src/haxcms-party.js
@@ -12,13 +12,13 @@ export class HaxCMSParty extends DDD {
 
     constructor() {
         super();
-
+        
     }
 
     static get styles() {
         return [
             super.styles,
-            css`
+            css `
             
             `
         ]
@@ -28,25 +28,34 @@ export class HaxCMSParty extends DDD {
         return html `
             <div class='add-members'>
                 <h2>Add Members</h2>
-                <div class='add-input'>
-                    <input type='text'></input>
-                    <button id='add-user-btn'>Add User</button>
+                <div class='add-input'> <!-- User will input a name for their party member, which will go through addUser() to generate a character -->
+                    <input type='text' id='add-user-text'></input>
+                    <button id='add-user-btn' @click=${this.addUser}>Add User</button>
                 </div>
             </div>
-            <div class='party-section'>
+            <div class='party'>
                 <h2>Your Current Party</h2>
-                <div class='party-members'>
+                <div class='party-showcase'> <!-- This section will be a showcase of the 'party members', users can also remove party members -->
 
                 </div>
             </div>
             <div class='confirmation-control'>
-
+                <button id='save'>Save</button>
+                <button id='cancel'>Cancel</button>
             </div>
         `
     }
 
     addUser() {
-
+        /* 
+        TODO:
+            1. Pull name from user input, which is stored into newPartyMember to be used for RPG character name.
+            2. Clear name from text input
+            3. Create tag for new rpg-character, using newPartyMember to assign the name of the RPG character.
+        */
+        const partyShowcase = document.querySelector('haxcms-party').shadowRoot.querySelector('.party .party-showcase');
+        let newPartyMember = document.querySelector('haxcms-party').shadowRoot.querySelector('#add-user-text').value;
+        console.log(newPartyMember);
     }
 
     static get properties() {
