6c71d5d5170de85e15f05db2aede6fb5d45830a8
zdodson21 <zdodson21@gmail.com>
2024-04-02 11:31:11 -0400

    started working with css

--- src/haxcms-party.js
+++ src/haxcms-party.js
@@ -1,6 +1,6 @@
-import { html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 import "@lrnwebcomponents/rpg-character/rpg-character.js";
+import { css, html } from 'lit';
 
 
 
@@ -28,6 +28,14 @@ export class HaxCMSParty extends DDD {
                     font-family: "Press Start 2P", var(--ddd-font-primary);
                 }
                 
+                .haxcms-party-container {
+                    /* border-style: solid; */
+                    border: var(--ddd-border-md);
+                    border-color: black;
+                    padding: var(--ddd-spacing-2);
+                    width: 50%;
+                }
+                
                 .add-input {
                     text-align: center;
                     
@@ -39,10 +47,32 @@ export class HaxCMSParty extends DDD {
                     justify-content: center;
                     align-items: center;
                 }
-                
-                .party-showcase {
+
+                .party {
                     display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center
+                }
+
+                .party-showcase {
+                    max-width: 575px;
+                    min-width: 0px;
+                    display: flex;
+                    justify-content: center;
                     flex-wrap: wrap;
+                }
+
+                .border {
+                    border-style: solid;
+                }
+
+                .user-character {
+                    display: flex;
+                    justify-content: center;
+                    width: 115px;
+                    flex-wrap: wrap;
+                    flex-direction: column;
                 }
 
                 .confirmation-control {
@@ -77,45 +107,47 @@ export class HaxCMSParty extends DDD {
 
     render() {
         return html `
-            <div class='add-members'>
-                <h2 style='text-align: center;'>Add Members</h2>
-                <form class='add-input' @submit=${this.addUser}> <!-- User will input a name for their party member, which will go through addUser() to generate a character -->
-                    <div class='add-party-member'>
-                        <label for="character-name">Party Member Name:</label><br>
-                        <input type='text' name='character-name' placeholder='Type Name Here...' id='add-user-text'/>
-                        <input type="submit" value='Add User'> <br>
-                    </div>
-                    <div class='select-hat'>
-                        <select name="hats" id="hat-select">
-                            <option value="none">--Please Select a Hat--</option>
-                            <option value="bunny">Bunny</option>
-                            <option value="coffee">Coffee</option>
-                            <option value="construction">Construction</option>                        
-                            <option value="cowboy">Cowboy</option>
-                            <option value="education">Education</option>
-                            <option value="knight">Knight</option>
-                            <option value="ninja">Ninja</option>
-                            <option value="party">Party</option>
-                            <option value="pirate">Pirate</option>
-                            <option value="watermelon">Watermelon</option>
-                        </select>
-                    </div>
-                </form>
-            </div>
-            <div class='party'>
-                <h2>Your Current Party</h2>
-                <div class='party-showcase'>
-                    ${this.partyMembers.map((rpgCharacter) => html`
-                        <div class='user-character'>
-                            <rpg-character seed="${rpgCharacter.seed}" hat='${rpgCharacter.hat}' id='rpg-${rpgCharacter.id}' class='${rpgCharacter.seed}'></rpg-character> 
-                            <p style='text-align: center' class='${rpgCharacter.seed}'>${rpgCharacter.seed}</p>
-                            <button id='delete-btn' rpgID='${rpgCharacter.id}' @click=${this.removeUser}>Delete</button>
+            <div class='haxcms-party-container'>
+                <div class='add-members'>
+                    <h2 style='text-align: center;'>Add Members</h2>
+                    <form class='add-input' @submit=${this.addUser}> <!-- User will input a name for their party member, which will go through addUser() to generate a character -->
+                        <div class='add-party-member'>
+                            <label for="character-name">Party Member Name:</label><br>
+                            <input type='text' name='character-name' placeholder='Type Name Here...' id='add-user-text'/>
+                            <input type="submit" value='Add User'> <br>
                         </div>
-                    `)}
+                        <div class='select-hat'>
+                            <select name="hats" id="hat-select">
+                                <option value="none">--Please Select a Hat--</option>
+                                <option value="bunny">Bunny</option>
+                                <option value="coffee">Coffee</option>
+                                <option value="construction">Construction</option>                        
+                                <option value="cowboy">Cowboy</option>
+                                <option value="education">Education</option>
+                                <option value="knight">Knight</option>
+                                <option value="ninja">Ninja</option>
+                                <option value="party">Party</option>
+                                <option value="pirate">Pirate</option>
+                                <option value="watermelon">Watermelon</option>
+                            </select>
+                        </div>
+                    </form>
                 </div>
-            </div>
-            <div class='confirmation-control'>
-                <button id='save' @click=${this.saveParty}>Save</button>
+                <div class='party'>
+                    <h2>Your Current Party</h2>
+                    <div class='party-showcase'>
+                        ${this.partyMembers.map((rpgCharacter) => html`
+                            <div class='user-character'>
+                                <rpg-character seed="${rpgCharacter.seed}" hat='${rpgCharacter.hat}' id='rpg-${rpgCharacter.id}' class='${rpgCharacter.seed}'></rpg-character> 
+                                <p style='text-align: center' class='${rpgCharacter.seed}'>${rpgCharacter.seed}</p>
+                                <button id='delete-btn' rpgID='${rpgCharacter.id}' @click=${this.removeUser}>Delete</button>
+                            </div>
+                        `)}
+                    </div>
+                </div>
+                <div class='confirmation-control'>
+                    <button id='save' style='margin-top: var(--ddd-spacing-2);' @click=${this.saveParty}>Save</button>
+                </div>
             </div>
         `
     }
@@ -158,6 +190,7 @@ export class HaxCMSParty extends DDD {
             this.requestUpdate();
         }
         this.rpgId += 1;
+        this.borderController();
     }
 
 
@@ -229,11 +262,21 @@ export class HaxCMSParty extends DDD {
             alert('Saved party');
         }
 
-        
+        this.borderController();
         // needs to be able to to save to local storage any party member that is not part of a 'removed' class
     }
 
     // create a "reset" debug command, comment out when done, for testing purposes only???
+
+    borderController() {
+        const showcase = document.querySelector('haxcms-party').shadowRoot.querySelector('.party-showcase');
+        
+        if (this.partyMembers.length === 0) {
+            showcase.classList.remove('border');
+        } else {
+            showcase.classList.add('border');
+        }
+    }
 
     static get properties() {
         return {
